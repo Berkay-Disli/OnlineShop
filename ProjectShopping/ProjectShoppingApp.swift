dd48f6aa6ce0748fda29413ebb0baf677ca45e96
Berkay-Disli <berkaydisli.work@gmail.com>
2022-04-18 15:00:06 +0300

    Initial Commit

---
+++ ProjectShopping/ProjectShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectShoppingApp.swift
+//  ProjectShopping
+//
+//  Created by Berkay Disli on 18.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ProjectShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
